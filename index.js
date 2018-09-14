0fa1efdfca4a93efde194f5a8a9329802c086e89
Denise Hastings <dehast59@gmail.com>
2018-09-14 14:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
 const app = "I don't do much."
 // my code is below here
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
- 
-    dodger.style.left = `${left - 1}px`
-  }
-})
-
-var dodger = document.getElementById('dodger')
-
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
- 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
