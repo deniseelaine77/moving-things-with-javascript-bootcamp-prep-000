a2195c66db8ab532480652335e468bfb80aadc30
Denise Hastings <dehast59@gmail.com>
2018-09-14 15:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', "")
-  var right = parseInt(rightNumbers, 10)
+  var rightNumbers = dodger.style.left.replace('px', "")
+  var left = parseInt(rightNumbers, 10)
   console.log("we are moving right")
-  //if(right < 360) {
-    dodger.style.right = `${right + 1}px`
+  //if(left < 360) {
+    dodger.style.left = `${left + 1}px`
  // }
 }
 document.addEventListener('keydown', function(e) {
