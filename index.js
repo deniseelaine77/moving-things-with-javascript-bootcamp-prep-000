90662d17168b30e847fed7f77e4c9f9a72d93625
Denise Hastings <dehast59@gmail.com>
2018-09-14 15:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 const app = "I don't do much."
 // my code is below here
-var dodger = document.getElementById('dodger')
 
-document.addEventListener('keydown', function(e) {
-  if(e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10);
-    dodger.sytle.left = `${left - 1}px`
-    }
-})
+
