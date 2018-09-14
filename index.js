a0626ae12ead3c15b068c97a0a2a056343aa3a5b
Denise Hastings <dehast59@gmail.com>
2018-09-14 14:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 // my code is below here
 document.addEventListener('keydown', function(e) {
-  if(e.which === 39)
+  if(e.which === 39){
   var rightNumbers = dodger.style.left.replace('px','')
   var right = parseInt(rightNumbers, 10)
   dodger.style.right = `$(right =1)px`
