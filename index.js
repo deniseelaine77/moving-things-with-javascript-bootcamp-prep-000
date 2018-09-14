ddeeb2ce7e66de064def2bd040dac2ceddee3bd6
Denise Hastings <dehast59@gmail.com>
2018-09-14 15:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.left.replace('px', "")
   var right = parseInt(rightNumbers, 10)
   
-  if(right < 360) {
+  //if(right < 360) {
     dodger.style.right = `${right + 1}px`
-  }
+ // }
 }
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
