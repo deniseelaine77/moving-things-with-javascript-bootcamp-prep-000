81fe1afddb30827ef766a3935961a65e8f0bc086
Denise Hastings <dehast59@gmail.com>
2018-09-14 15:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = "I don't do much."
 // my code is below here
+var dodger = document.getElementById('dodger')
+
 document.addEventListener('keydown', function(e) {
   if(e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
